ea067ae516e71b831cd9e941d4ac2273557af903
AnthonyTB <anthonylol@protonmail.com>
2020-08-30 10:14:17 -0700

    implemented reach router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
+      <header id="something-important">
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
