2e315eef93c34f1962f4515bafbfa8d6f6cce1c3
AnthonyTB <anthonylol@protonmail.com>
2020-08-31 16:13:40 -0700

    learned the benefits of lazy loading components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
-import { Router } from "@reach/router";
-import Details from "./Details";
+import { Router, Link } from "@reach/router";
 import ThemeContext from "./ThemeContext";
-import NavBar from "./NavBar";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const themeHook = useState({ buttonColor: "darkBlue", modalColor: "pink" });
   return (
     <ThemeContext.Provider value={themeHook}>
       <div>
-        <NavBar />
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Suspense fallback={<h1>loading route...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
